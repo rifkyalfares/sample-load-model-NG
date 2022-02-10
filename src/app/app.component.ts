889022d30a190de596c5e1fac7c44d69ff58c652
Muhamad Rifky <muhamad210501@gmail.com>
2022-02-10 13:46:17 +0700

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
 import { Component } from '@angular/core';
 
+
+declare const BABYLON:any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Sample-Load-Model-NG';
+  	title = 'Sample-Load-Model-NG';
+
+	ngAfterViewInit(){
+		var canvas:any = document.getElementById("renderCanvas");
+
+		var startRenderLoop = function (engine:any, canvas:any) {
+			engine.runRenderLoop(function () {
+				if (sceneToRender && sceneToRender.activeCamera) {
+					sceneToRender.render();
+				}
+			});
+		}
+
+		var engine:any = null;
+		var scene:any = null;
+		var sceneToRender:any = null;
+		var createDefaultEngine = function() { 
+			return new BABYLON.Engine(canvas, true, { 
+				preserveDrawingBuffer: true,
+				stencil: true,  
+				disableWebGL2Support: false
+			}); 
+		};
+		var createScene = function () {
+			var scene = new BABYLON.Scene(engine);
+
+			var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 1, 3), scene);
+			camera.setTarget(new BABYLON.Vector3(0, 1, 0));
+			camera.attachControl(canvas, true);
+
+			var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+			light.intensity = 2;
+			camera.speed = 0.1
+
+			console.log("loading object...")
+			BABYLON.SceneLoader.ImportMesh(
+				"",
+				"",
+				"assets/model.glb",
+				scene,
+				()=>{
+					console.log("load object done!")
+				}
+			)
+			return scene;
+		};
+		const initFunction = async function() {    
+			var asyncEngineCreation = async function() {
+				try {
+					return createDefaultEngine();
+				} catch(e) {
+					console.log("the available createEngine function failed. Creating the default engine instead");
+					return createDefaultEngine();
+				}
+			}
+
+			engine = await asyncEngineCreation();
+			if (!engine) throw 'engine should not be null.';
+			startRenderLoop(engine, canvas);
+			scene = createScene();
+		};
+		initFunction().then(() => {sceneToRender = scene});
+
+		// Resize
+		window.addEventListener("resize", function () {
+			engine.resize();
+		});
+	}
 }
